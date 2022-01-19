6b3e6d82214e281161a5fd408141519f92f066bc
Karim Pazoki <pazoki.karim72@gmail.com>
2022-01-19 15:09:43 +0330

    Add service provider

---
+++ src/LaravelJwtAuthServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kaafochino\LaravelJwtAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelJwtAuthServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
